c9473885bbe4b597a41ad00b3362804ea11c6051
David Goligorsky <d.goligorsky@gmail.com>
2018-12-09 17:38:07 -0500

    set up particle field and camera rotations

--- js/planet.js
+++ js/planet.js
@@ -9,6 +9,8 @@ const sectionTag = document.querySelector("section")
 sectionTag.appendChild(renderer.domElement)
 
 const scene = new THREE.Scene()
+// scene.fog = new THREE.Fog(0x000000, 0.1, 7000)
+scene.fog = new THREE.FogExp2(0x000000, 0.00025)
 
 // add some lighting
 const ambientLight = new THREE.AmbientLight(0x777777)
@@ -17,12 +19,11 @@ const pointLight = new THREE.PointLight(0xffffff, 1, 0)
 pointLight.position.set(500, 500, -2000)
 scene.add(pointLight)
 
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 0.1, 5000)
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 0.1, 7000)
 camera.position.z = -3000
 
 // make a THREE.js loader
 const loader = new THREE.TextureLoader()
-
 
 // make the planet
 const makePlanet = function(){
@@ -51,15 +52,88 @@ const makeRing = function(width, color) {
     return mesh
 }
 
+// make a particle system
+const makeStars = function() {
+    const texture = loader.load("assets/particle.png")
+    const geometry = new THREE.Geometry()
+
+    for(let i = 0; i < 5000; i = i + 1) {
+        const point = new THREE.Vector3()
+        const sphericalPoint = new THREE.Spherical(
+            // radius, phi, theta
+            900 + Math.random() * 900,
+            2 * Math.PI * Math.random(),
+            Math.PI * Math.random()
+        )
+
+        point.setFromSpherical(sphericalPoint)
+
+        geometry.vertices.push(point)
+    }
+
+    const material = new THREE.PointsMaterial({
+        color: 0xffffff,
+        size: 50,
+        map: texture,
+        transparent: true,
+        blending: THREE.AdditiveBlending,
+        depthTest: true,
+        depthWrite: false
+    })
+
+    const points = new THREE.Points(geometry, material)
+    scene.add(points)
+    return points
+}
+
+// // make a curved line
+// const makeLine = function() {
+//     const path = new THREE.QuadraticBezierCurve3(
+//         new THREE.Vector3(800, 0, 0),
+//         new THREE.Vector3(1200, 0, -1200),
+//         new THREE.Vector3(0, 0, -800)
+//     )
+
+//     const geometry = new THREE.TubeGeometry(path, 50, 8, 20, false)
+//     const material = new THREE.MeshBasicMaterial({
+//         color: 0xff0000
+//     })
+
+//     const mesh = new THREE.Mesh(geometry, material)
+//     scene.add(mesh)
+//     return mesh
+// }
+
 const earth = makePlanet()
 const ring1 = makeRing(1100, 0xff4141)
 const ring2 = makeRing(1200, 0xffffff)
 const ring3 = makeRing(1300, 0xffdb00)
+const stars = makeStars()
+// const line = makeLine()
+
+
+// hold the camera positions for tweening
+let currentX = 0
+let currentY = 0
+let aimX = 0
+let aimY = 0
 
 const animate = function() {
+    
+    // tweening the camrea moves on mouse move
+        const diffX = aimX - currentX
+        const diffY = aimY - currentY
+
+        currentX = currentX + diffX * 0.05
+        currentY = currentY + diffY * 0.05
+
+        camera.position.x = currentX
+        camera.position.y = currentY
+
     camera.lookAt(scene.position)
 
     earth.rotateY(0.01)
+    // line.rotateY(0.01)
 
     ring1.geometry.rotateY(0.004)
     ring2.geometry.rotateY(-0.002)
@@ -85,3 +159,33 @@ window.addEventListener("resize", function() {
 
 //     earth.rotation.set(0, scrollPosition * 0.01, 0)
 // })
+
+let isMouseDown = false
+// let startX = 0
+// let startY = 0
+
+// document.addEventListener("mousedown", function(event) {
+//     isMouseDown = true
+//     startX = event.pageX
+//     startY = event.pageY
+// })
+
+// document.addEventListener("mouseup", function() {
+//     isMouseDown = false
+// })
+
+document.addEventListener("mousemove", function(event) {
+    // if (isMouseDown) {
+        // aimX = aimX + ((event.pageX - startX) * 8)
+        // aimY = aimY + ((event.pageY - startY) * 8)
+        // startX = event.pageX
+        // startY = event.pageY
+    // }
+    aimX = ((window.innerWidth / 2) - event.pageX) * 4
+    aimY = ((window.innerHeight / 2) - event.pageY) * 4 
+})
+
+document.addEventListener("touchmove", function(event) {
+    aimX = ((window.innerWidth / 2) - event.pageX) * 4
+    aimY = ((window.innerHeight / 2) - event.pageY) * 4 
+})
